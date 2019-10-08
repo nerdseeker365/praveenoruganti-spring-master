0ba1248e5de2709cf82ba7f1c59393e893d5d5bb
praveen <praveen@praveen-PC>
2019-10-08 23:55:52 +0530

    updated

--- praveenoruganti-springmvc-master/src/com/praveen/bean/User.java
+++ praveenoruganti-springmvc-master/src/com/praveen/bean/User.java
@@ -5,25 +5,29 @@ public class User {
 	private String password;
 	private String userName;
 	private int ssoId;
+
 	public String getPassword() {
 		return password;
 	}
+
 	public void setPassword(String password) {
 		this.password = password;
 	}
+
 	public String getUserName() {
 		return userName;
 	}
+
 	public void setUserName(String userName) {
 		this.userName = userName;
 	}
+
 	public int getSsoId() {
 		return ssoId;
 	}
+
 	public void setSsoId(int ssoId) {
 		this.ssoId = ssoId;
 	}
-
-	
 
 }
--- praveenoruganti-springmvc-master/src/com/praveen/config/WebMvcConfig.java
+++ praveenoruganti-springmvc-master/src/com/praveen/config/WebMvcConfig.java
@@ -1,12 +1,16 @@
 package com.praveen.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 import org.springframework.web.servlet.view.JstlView;
+
+import com.praveen.interceptor.RequestHeaderInterceptor;
 
 @Configuration
 @EnableWebMvc
@@ -22,5 +26,13 @@ public class WebMvcConfig implements WebMvcConfigurer {
       return resolver;
    }
   
+   @Autowired
+	private RequestHeaderInterceptor requestHeaderInterceptor;
+
+	@Override
+	public void addInterceptors(InterceptorRegistry registry) {
+		// TODO Auto-generated method stub
+		registry.addInterceptor(requestHeaderInterceptor);
+	}
 
 }
